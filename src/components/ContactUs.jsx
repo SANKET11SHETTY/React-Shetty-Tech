8e0f72e1f70c480488b7716f32f0afe0a36e8699
SANKET SHETTY <legendhimself007@gmail.com>
2024-08-27 14:18:39 +0530

    Connection to Firebase
    Added backend database connection via Firebase

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,14 +5,112 @@ import Alert from 'react-bootstrap/Alert';
 
 const ContactUs = () => {
  const [text, setText] = useState("");
+ const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
 
- const alerted = () => {
-  setText(<Container><Alert dismissible variant='success'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill " viewBox="0 0 16 16">
-    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-  </svg> &nbsp; 
-  Your details are successfully submitted
-        </Alert></Container> );
- }
+  let name, value;
+  const postUserData = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const submitData = async (e) => {
+    e.preventDefault();
+    const { name, email, number, message } = userData;
+
+    if (name && email && number && message) {
+      const res = await fetch(
+        "https://shettytechcontact-default-rtdb.firebaseio.com/userDataRecord.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            number,
+            message,
+          }),
+        }
+      );
+      if (res) {
+        setUserData({
+          name: "",
+          email: "",
+          number: "",
+          message: "",
+        });
+        setText(
+          <Container>
+            <Alert dismissible variant="success ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-check-circle-fill "
+                viewBox="0 0 16 16"
+              >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+              </svg>{" "}
+              &nbsp; Your details are successfully submitted
+            </Alert>
+          </Container>
+        );
+      } else {
+        setText(
+          <Container>
+            <Alert dismissible variant="danger ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-exclamation-triangle-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+              </svg>{" "}
+              &nbsp; Please enter your Details.!
+            </Alert>
+          </Container>
+        );
+      }
+    } else {
+      setText(
+        <Container>
+          <Alert dismissible variant="danger">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-exclamation-triangle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+            </svg>{" "}
+            &nbsp; Please Enter the Details.!
+          </Alert>
+        </Container>
+      );
+    }
+  };
+
+ // const alerted = () => {
+ //  setText(<Container><Alert dismissible variant='success'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill " viewBox="0 0 16 16">
+ //    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+ //  </svg> &nbsp; 
+ //  Your details are successfully submitted
+ //        </Alert></Container> );
+ // }
    
   return (
     
@@ -56,13 +154,15 @@ const ContactUs = () => {
 
       <div className="col-lg-6">
         <div className="container form-section">
-          <form className="row g-3" action="contact.html" method="post">
+          <form className="row g-3"  method="POST">
             <div className="mb-3">
-              <label for="name" className="form-label">NAME</label>
-              <input type="text" className="form-control" id="name" name="name" />
+              <label htmlFor="name" className="form-label">NAME</label>
+              <input type="text" className="form-control" id="name" name="name" value={userData.name}
+                    onChange={postUserData}
+                  /> />
             </div>
             <div className="mb-3">
-              <label for="exampleFormControlInput1" className="form-label"
+              <label htmlFor="exampleFormControlInput1" className="form-label"
                 >EMAIL</label
               >
               <input
@@ -70,10 +170,13 @@ const ContactUs = () => {
                 className="form-control"
                 id="exampleFormControlInput1"
                 name="email"
+               value={userData.email}
+                    onChange={postUserData}
+                  />
               />
             </div>
             <div className="mb-3">
-              <label for="exampleFormControlInput1" className="form-label"
+              <label htmlFor="exampleFormControlInput1" className="form-label"
                 >PHONE NUMBER</label
               >
               <input
@@ -81,10 +184,13 @@ const ContactUs = () => {
                 className="form-control"
                 id="exampleFormControlInput1"
                 name="number"
+               value={userData.number}
+                    onChange={postUserData}
+                  />
               />
             </div>
             <div className="mb-3">
-              <label for="exampleFormControlTextarea1" className="form-label"
+              <label htmlFor="exampleFormControlTextarea1" className="form-label"
                 >MESSAGE</label
               >
               <textarea
@@ -92,12 +198,15 @@ const ContactUs = () => {
                 id="exampleFormControlTextarea1"
                 rows="7"
                 name="message"
+               value={userData.message}
+                    onChange={postUserData}
+                  />
               ></textarea>
             </div>
             <div className="btn-form pb-4">
               <button
-                type="button"
-                className="btn btn-primary rounded-3 px-3 fw-semibold" onClick={alerted}
+                type="submit"
+                className="btn btn-primary rounded-3 px-3 fw-semibold" onClick={submitData}
               >
                 SUBMIT
               </button>
